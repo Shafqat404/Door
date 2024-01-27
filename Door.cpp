2796352dd9a5848106bfad2f827dc55d297a9667
Shafqat Ali <shafqat182404@gmail.com>
2024-01-27 22:46:46 +0500

    Inilitize the door class prototype

---
+++ Door.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Door {
+    private:
+    bool isopen;
+    bool isLocked;
+public:
+    Door(); 
+    void openDoor();
+    void closeDoor();
+    void lockDoor();
+    void unlock();
+    bool isDoorOpen() const;
+    bool isDoor Locked() const;
+};
+int main(){
+return 0;
+
+}
+
+
+
+
+
